dd340f5061d6b8bd4510c6b88b6fc0c6d68a90fb
Navneet Singh Negi <navneetsn18@gmail.com>
2023-04-25 00:01:36 +0530

    🐛 BUG: Added Proxies for Amazon

--- modules/extractor/Amazon.py
+++ modules/extractor/Amazon.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-
+from fp.fp import FreeProxy
 
 def getAmazonData(url):
     HEADERS = ({'User-Agent':
                 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
                 'Accept-Language': 'en-US, en;q=0.5'})
-    response = requests.get(url, headers=HEADERS)
+    proxy = FreeProxy(country_id=['IN'], rand=True).get()
+    response = requests.get(url, headers=HEADERS, proxies={"http": proxy, "https": proxy})
     soup = BeautifulSoup(response.text, 'html.parser')
 
     data = {
